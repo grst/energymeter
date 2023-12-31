3720710170135cbe0577f5f44787a378523bba84
Gregor Sturm <mail@gregor-sturm.de>
2023-12-31 14:31:58 +0100

    Monitor all inputs configured in yaml

--- monitor.py
+++ monitor.py
@@ -4,22 +4,22 @@ from RPi import GPIO
 import yaml
 import datetime
 import time
+from functools import partial
+from threading import Event
 
-with open("./config.yml") as f:
-    CONFIG = yaml.safe_load(f)
-print(CONFIG)
+def pulse_callback(channel, *, meter_id = None):
+    print(f"Pulse on {meter_id} at {datetime.datetime.utcnow().isoformat()}")
 
-PIN = 13
+if __name__ == "__main__":
+    with open("./config.yml") as f:
+        CONFIG = yaml.safe_load(f)
+    
+    GPIO.setmode(GPIO.BCM)
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    for meter_id, params in CONFIG["meters"].items():
+        print(f"Setting up meter '{meter_id}' on GPIO {params['GPIO']}")
+        GPIO.setup(params["GPIO"], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(params["GPIO"], GPIO.FALLING, callback=partial(pulse_callback, meter_id=meter_id))
 
-
-def pulse_callback(channel):
-    print(f"Pulse at {datetime.datetime.utcnow().isoformat()}")
-
- 
-GPIO.add_event_detect(PIN, GPIO.FALLING, callback=pulse_callback)
-
-while True:
-    time.sleep(0.1)
+    event = Event()
+    event.wait()
